d7fe4a3c86d48b5511104dc20338644fef2130c9
Darrin Cates <darrincates333@gmail.com>
2018-06-16 19:36:19 -0500

    homework upload

---
+++ app.py
@@ -0,0 +1,18 @@
+# import necessary libraries
+from flask import Flask, render_template
+
+# create instance of Flask app
+app = Flask(__name__)
+
+
+# create route that renders index.html template
+@app.route("/")
+def index():
+    #player_dictionary = {"player_1": "Jessica",
+    #                     "player_2": "Mark"}
+    #return render_template("index.html", dict=player_dictionary)
+    x = "etste"
+    y = 'asdf'
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,7 @@
+# Twitter API Keys
+consumer_key = "6gle0A7idLJ6buHExczFkcITn"
+consumer_secret = "EfUO9dEERYvqVArOkX8FHMm316QABh8Hfjdf6VJUXpnju1NRrq"
+access_token = "751039949382746112-u6XL2EE9ANEzERwNI8stFtIyxQ776W0"
+access_token_secret = "uKUfmpPYUnNATuNEyM9qervAzQi4LxJ9Ns5D24ZlCdNOz"
+
+
